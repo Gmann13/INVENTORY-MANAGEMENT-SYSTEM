a1d389cba86c475fe18375c58520ba5db4481aa1
Yash Savaliya <128933983+0Mahakal@users.noreply.github.com>
2023-04-14 21:57:16 -0400

    Update PACKAGES.sql
    STOCK PACKAGE

--- PACKAGES.sql
+++ PACKAGES.sql
@@ -129,3 +129,123 @@ PROCEDURE DELETE_SUPPLIERS
 
 END SUPPLIER_PKG;
 
+----------STOCK_PACKAGE-----------
+CREATE OR REPLACE EDITIONABLE PACKAGE  STOCK_PKG
+AS 
+
+PROCEDURE UPDATE_STOCK
+(
+P_STOCK_ID IN STOCK.STOCK_ID%TYPE,
+P_STOCK_NAME IN STOCK.STOCK_NAME%TYPE,
+P_STOCK_QUANTITY IN STOCK.STOCK_QUANTITY%TYPE,
+P_REORDER_LEVEL IN STOCK.REORDER_LEVEL%TYPE,
+P_STOCK_PRICE IN STOCK.STOCK_PRICE%TYPE,
+P_SPECS IN STOCK.SPECS%TYPE,
+P_STOCK_SUPPLIER_ID IN STOCK.STOCK_SUPPLIER_ID%TYPE  , 
+P_STOCK_IN_DATE IN STOCK.STOCK_IN_DATE%TYPE   ,
+P_MANUFACTURER_NAME  IN STOCK.MANUFACTURER_NAME%TYPE  );
+
+PROCEDURE DELETE_STOCK
+(
+P_STOCK_ID IN STOCK.STOCK_ID%TYPE); 
+
+END STOCK_PKG;
+
+
+--------STOCK_BODY--------------
+
+
+CREATE OR REPLACE EDITIONABLE PACKAGE BODY  STOCK_PKG
+AS 
+PROCEDURE UPDATE_STOCK
+(
+P_STOCK_ID IN STOCK.STOCK_ID%TYPE,
+P_STOCK_NAME IN STOCK.STOCK_NAME%TYPE,
+P_STOCK_QUANTITY IN STOCK.STOCK_QUANTITY%TYPE,
+P_REORDER_LEVEL IN STOCK.REORDER_LEVEL%TYPE,
+P_STOCK_PRICE IN STOCK.STOCK_PRICE%TYPE,
+P_SPECS IN STOCK.SPECS%TYPE,
+P_STOCK_SUPPLIER_ID IN STOCK.STOCK_SUPPLIER_ID%TYPE  , 
+P_STOCK_IN_DATE IN STOCK.STOCK_IN_DATE%TYPE   ,
+P_MANUFACTURER_NAME IN STOCK.MANUFACTURER_NAME%TYPE  )
+
+AS
+ v_stock_quantity NUMBER;
+    v_stock_price NUMBER;
+    v_stock_name NUMBER;
+    v_stock_supplier_id NUMBER;
+    v_stock_in_date NUMBER;
+    EX_RAISE_INVALID_STOCK_QUANTITY EXCEPTION;
+    EX_RAISE_INVALID_STOCK_PRICE EXCEPTION;
+    EX_RAISE_INVALID_STOCK_NAME EXCEPTION;
+    EX_RAISE_INVALID_STOCK_SUPPLIER_ID EXCEPTION;
+    EX_RAISE_INVALID_STOCK_IN_DATE EXCEPTION;
+    
+BEGIN
+-----------EXCEPTION QUERY----------------
+SELECT COUNT(*) INTO V_STOCK_QUANTITY FROM STOCK WHERE stock_quantity = p_stock_quantity;
+SELECT COUNT(*) INTO V_STOCK_PRICE FROM STOCK WHERE stock_price = p_stock_price;
+SELECT COUNT(*) INTO V_STOCK_NAME FROM STOCK WHERE stock_name = p_stock_name;
+SELECT COUNT(*) INTO V_STOCK_SUPPLIER_ID FROM STOCK WHERE stock_supplier_id = p_stock_supplier_id;
+SELECT COUNT(*) INTO V_STOCK_IN_DATE FROM STOCK WHERE stock_in_date = p_stock_in_date;
+IF V_STOCK_quantity = NULL THEN
+      RAISE  EX_RAISE_INVALID_STOCK_QUANTITY;
+    END IF;
+IF V_STOCK_price = NULL THEN
+      RAISE  EX_RAISE_INVALID_STOCK_PRICE;
+    END IF;
+IF V_STOCK_name = NULL THEN
+      RAISE  EX_RAISE_INVALID_STOCK_name;
+    END IF;
+IF V_STOCK_supplier_id = NULL THEN
+      RAISE  EX_RAISE_INVALID_STOCK_supplier_id;
+    END IF;
+IF V_STOCK_in_date = NULL THEN
+      RAISE  EX_RAISE_INVALID_STOCK_in_date;
+    END IF;
+-----------UPDATING STOCK VALUE----------------
+UPDATE STOCK SET stock_name = p_stock_name,
+stock_quantity = p_stock_quantity,
+reorder_level = p_reorder_level,
+stock_price = p_stock_price,
+stock_supplier_id = p_stock_supplier_id,
+stock_in_date = p_stock_in_date
+where stock_id = p_stock_id; 
+
+
+EXCEPTION
+      WHEN EX_RAISE_INVALID_STOCK_QUANTITY THEN
+      dbms_output.put_line('stock_quantity cant be NULL. Please use another value.');
+      RETURN;
+      WHEN EX_RAISE_INVALID_STOCK_PRICE THEN
+      dbms_output.put_line('stock_price cant be NULL. Please use another value.');
+      RETURN;
+      WHEN EX_RAISE_INVALID_STOCK_NAME THEN
+      dbms_output.put_line('stock_name cant be NULL. Please use another value.');
+      RETURN;
+      WHEN EX_RAISE_INVALID_STOCK_SUPPLIER_ID THEN
+      dbms_output.put_line('stock_supplier_id cant be NULL. Please use another value.');
+      RETURN;
+      WHEN EX_RAISE_INVALID_STOCK_IN_DATE THEN
+      dbms_output.put_line('stock_in_date cant be NULL. Please use another value.');
+      RETURN;
+      END;
+PROCEDURE DELETE_STOCK
+(
+P_STOCK_ID IN STOCK.STOCK_ID%TYPE)
+
+AS
+ V_STOCK_ID NUMBER;
+    INVALID_STOCK_ID EXCEPTION;
+               begin 
+                SELECT COUNT(*) INTO V_STOCK_ID FROM STOCK WHERE STOCK_ID = p_stock_id;
+               if  V_STOCK_ID > 0  then
+            raise INVALID_STOCK_ID;
+            END IF;
+               delete from stock where stock_id = p_stock_id;
+                EXCEPTION
+    WHEN INVALID_STOCK_ID THEN
+    DBMS_OUTPUT.PUT_LINE('STOCK ' ||  V_STOCK_ID || ' DOES NOT EXIST! CANNOT DELETE');    
+               end;
+      
+END STOCK_PKG;
